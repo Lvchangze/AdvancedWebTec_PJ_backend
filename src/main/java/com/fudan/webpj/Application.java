e67be2911b3ddf820762b67d6ec38f0f5eedeb68
Lvchangze <18302010026@fudan.edu.cn>
2021-03-16 21:00:44 +0800

    1

--- src/main/java/com/fudan/webpj/Application.java
+++ src/main/java/com/fudan/webpj/Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.fudan.webpj.repository")
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
