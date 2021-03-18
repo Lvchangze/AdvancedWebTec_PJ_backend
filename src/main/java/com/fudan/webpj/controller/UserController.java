b4cf82392be3b04723799d45a1398f5519c38286
Lvchangze <18302010026@fudan.edu.cn>
2021-03-18 20:21:42 +0800

    1

--- src/main/java/com/fudan/webpj/controller/UserController.java
+++ src/main/java/com/fudan/webpj/controller/UserController.java
@@ -2,9 +2,11 @@ package com.fudan.webpj.controller;
 
 import com.fudan.webpj.entity.User;
 import com.fudan.webpj.service.UserService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -17,9 +19,13 @@ public class UserController {
         this.userService = userService;
     }
 
-
     @RequestMapping("getUser/{id}")
     public User GetUser(@PathVariable int id){
         return userService.getUserById(id);
+    }
+
+    @RequestMapping("/register")
+    public String GetUser2(@RequestParam("id") int id){
+        return userService.getUserById(id).toString();
     }
 }
