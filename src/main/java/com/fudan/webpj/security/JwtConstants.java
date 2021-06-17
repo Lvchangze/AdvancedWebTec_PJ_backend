8711495781b313a322167b293aa93064e6c5aa54
Lvchangze <18302010026@fudan.edu.cn>
2021-06-17 21:58:08 +0800

    1

--- src/main/java/com/fudan/webpj/security/JwtConstants.java
+++ src/main/java/com/fudan/webpj/security/JwtConstants.java
@@ -1,6 +1,6 @@
 package com.fudan.webpj.security;
 
 public class JwtConstants {
-    //用于token的chaims保存有效信息字段名
+    //用于token的chains保存有效信息字段名
     public static final String JWT_KEY_USER_ID = "uid";
 }
